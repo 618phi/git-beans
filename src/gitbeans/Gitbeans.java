50a6788abd8e89aaef309ab857ca953eb10a4025
sadFrog <one.618phi.yahoo.co.uk>
2017-02-15 12:49:06 +0000

    f NetBeans commit 2

--- src/gitbeans/Gitbeans.java
+++ src/gitbeans/Gitbeans.java
@@ -4,6 +4,7 @@ public class Gitbeans {
 
     public static void main(String[] args) {
         System.out.println("Please work this time you shitter!!!!!!!");
+        System.out.println("create new repo on github, initialise with read-me");
     }
     
 }
