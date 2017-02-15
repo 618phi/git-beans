fec4394a8275970e630c91bee9762f5baf20e222
sadFrog <one.618phi.yahoo.co.uk>
2017-02-15 13:29:24 +0000

    NetBeans commit 2

--- src/gitbeans/Gitbeans.java
+++ src/gitbeans/Gitbeans.java
@@ -4,9 +4,11 @@ public class Gitbeans {
 
     public static void main(String[] args) {
         System.out.println("create new repo on github, initialise with read-me");
-        System.out.println("right click project-versioning-initialise git ropository");
+        System.out.println("Team-Git-Initialise");
         System.out.println("check root path click ok");
         System.out.println("Team-Commit");
+        System.out.println("Team-Remote-Pull-next-finish");
+        System.out.println("Team-Remote-Push-next-finish");
     }
     
 }
