584417a774e6323280f5103fa76c393fafa7c04f
sadFrog <one.618phi.yahoo.co.uk>
2017-02-15 13:04:04 +0000

    NetBeans commit 3

--- src/gitbeans/Gitbeans.java
+++ src/gitbeans/Gitbeans.java
@@ -3,8 +3,10 @@ package gitbeans;
 public class Gitbeans {
 
     public static void main(String[] args) {
-        System.out.println("Please work this time you shitter!!!!!!!");
         System.out.println("create new repo on github, initialise with read-me");
+        System.out.println("right click project-versioning-initialise git ropository");
+        System.out.println("check root path click ok");
+        System.out.println("Team-Commit");
     }
     
 }
